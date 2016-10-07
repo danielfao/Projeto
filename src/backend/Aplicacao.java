08e0f5a3e4f5ce10ea118de93215c5fd9836b1e3
Maisa Rissi <maisa.rissi@gmail.com>
2016-10-06 22:58:42 -0300

    teste Maisa

--- src/backend/Aplicacao.java
+++ src/backend/Aplicacao.java
@@ -4,7 +4,7 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World - Daniel - Develop");
+		System.out.println("Hello World - Maísa - Develop");
 
 	}
 
