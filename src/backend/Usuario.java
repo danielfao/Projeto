443076761151bd3a93a7661324f826a5125d0ffa
Daniel Fernandes <daniel.fao@gmail.com>
2016-10-08 16:21:30 -0300

    Criacao classe Veiculo #5;

---
+++ src/backend/Usuario.java
@@ -0,0 +1,29 @@
+package backend;
+
+import java.util.Date;
+
+public class Usuario extends Pessoa{
+	private int id;
+	private String login;
+	private String senha;
+	
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Usuario(String nome, String cpf, Date dataNascimento, String endereco, int telefone, int id, String login, String senha) {
+		super(nome, cpf, dataNascimento, endereco, telefone);
+		this.id = id;
+		this.login = login;
+		this.senha = senha;
+	}
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
