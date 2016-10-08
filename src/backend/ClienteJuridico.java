cd21b71bba63c0f69003ab41ff249e141aa2eae9
Daniel Fernandes <daniel.fao@gmail.com>
2016-10-08 16:20:11 -0300

    Criacao classe ClienteFisico #3;

---
+++ src/backend/ClienteJuridico.java
@@ -0,0 +1,56 @@
+package backend;
+
+public class ClienteJuridico {
+	private String razaoSocial;
+	private int cnpj;
+	private int telefone;
+	private String endereco;
+	
+	public ClienteJuridico() {
+		super();
+	}
+
+	public ClienteJuridico(String razaoSocial, int cnpj, int telefone, String endereco) {
+		super();
+		this.razaoSocial = razaoSocial;
+		this.cnpj = cnpj;
+		this.telefone = telefone;
+		this.endereco = endereco;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public int getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(int cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	public void cadastrarClienteJuridico (String razaoSocial, int cnpj, int telefone, String endereco) {
+		ClienteJuridico cliente = new ClienteJuridico();
+	}
+}
