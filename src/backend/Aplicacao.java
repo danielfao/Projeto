67c52d9a1f0edd353af6120e7d80dd9fa021f2b3
Daniel Fernandes <daniel.fao@gmail.com>
2016-10-08 16:21:50 -0300

    Update Aplicacao;

--- src/backend/Aplicacao.java
+++ src/backend/Aplicacao.java
@@ -4,8 +4,7 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World - Maisa");
-
+		System.out.println("Hello World - Projeto - SIGCor");
 	}
 
 }
