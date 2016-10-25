bd536c4ea08669faeb773ca719a3b38be902f3ef
Daniel Fernandes <daniel.fao@gmail.com>
2016-10-25 15:09:30 -0200

    Adicao de metodo gerarVendasTodosFuncionarios em relatorios e adicao de um novo usuario, venda e seguro testes #7;

--- src/backend/main/Aplicacao.java
+++ src/backend/main/Aplicacao.java
@@ -16,6 +16,8 @@ public class Aplicacao {
 
 	public static void main(String[] args) throws IOException {
 		List<Venda> vendas = new ArrayList<Venda>();
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+
 				
 		ClienteFisico clienteF1 = new ClienteFisico("Jose da Silva", "123", "09/10/1990", "(16)3355-5221", "Rua das Orquideas", 123, "Jd Paola", "13690-000", "Descalvado" , "SP", "Funcionario publico", "Casado");
 		//System.out.println(clienteF1);
@@ -27,15 +29,20 @@ public class Aplicacao {
 		//System.out.println(veiculo1);
 		Veiculo veiculo2 = new Veiculo("Corsa Hatch 1.6", "9BCHJ12H874637", "EPP-0908", 2009, 2010, 8907865, clienteJ1);
 		//System.out.println(veiculo2);
+		Veiculo veiculo3 = new Veiculo("Ford K 1.0", "9BCHJ32H874688", "HIJ-1617", 2013, 2014, 1005634, clienteJ1);
+		//System.out.println(veiculo3);
 
 		Seguro seguro1 = new Seguro(clienteF1, veiculo1, 6, "10/10/2015", "10/10/2016", "Porto Seguro");
 		//System.out.println(seguro1);
 		Seguro seguro2 = new Seguro(clienteF1, veiculo2, 3, "10/05/2016", "10/05/2017", "Mapfre");
 		//System.out.println(seguro2);
+		Seguro seguro3 = new Seguro(clienteJ1, veiculo3, 2, "11/03/2016", "11/03/2017", "Mapfre");
 		
 		Usuario usuario1 = new Usuario("Maisa Rissi", "234", "10/09/1991", "(16)99999-9999", "Rua das Alamedas", 345, "Ap-402", "13560-320", "Vila Monteiro", "Sao Carlos", "SP", "maisarissi", "123456");
 		//System.out.println(usuario1);
 		Usuario usuario2 = new Usuario("Sara Pauline", "457", "11/05/1990", "(16)99999-9999", "Rua das Orquideas", 4345, "13580-120", "Vila Nery", "Sao Carlos", "SP", "sarafabricio", "012983");
+		//System.out.println(usuario2);
+		Usuario usuario3 = new Usuario ("Daniel Fernandes", "333.222.444-01", "04/01/1985", "(16)98888-9999", "Rua dos Crisantemos", 454, "13560-060", "Santa Paula", "Sao Carlos", "SP", "danielfao", "12345" );
 		//System.out.println(usuario2);
 		
 		Venda venda = new Venda (usuario1, seguro1, 1234.50);
@@ -46,6 +53,14 @@ public class Aplicacao {
 		vendas.add(venda);
 		System.out.println(venda);
 		
+		venda = new Venda(usuario3, seguro3, 2050.20);
+		vendas.add(venda);
+		System.out.println(venda);
+		
+		usuarios.add(usuario1);
+		usuarios.add(usuario2);
+		usuarios.add(usuario3);
+		
 		seguro1.terSinistro();
 		seguro1.terSinistro();
 		
@@ -54,6 +69,8 @@ public class Aplicacao {
 		Relatorio relatorio = new Relatorio();
 		relatorio.gerarVendasPorFuncionario(usuario1, vendas);
 		relatorio.gerarVendasPorFuncionario(usuario2, vendas);
+		relatorio.gerarVendasPorFuncionario(usuario3, vendas);
+		relatorio.gerarVendasTodosFuncionarios(usuarios, vendas);
 	}
 
 }
--- src/backend/models/Relatorio.java
+++ src/backend/models/Relatorio.java
@@ -9,13 +9,14 @@ import java.util.List;
 
 public class Relatorio {
 	public void gerarVendasPorFuncionario(Usuario usuario, List<Venda> vendas) throws IOException{
-		Writer arquivo = new FileWriter("Vendas" + usuario.getNome() + ".txt");
+		Writer arquivo = new FileWriter("Vendas" + usuario.getNome().replaceAll(" ", "") + ".txt");
 		BufferedWriter buffer = new BufferedWriter (arquivo);
 		
 		boolean temVenda = false;
 		DecimalFormat valorFormat = new DecimalFormat("#.00");
-		buffer.write("==============\nVendas do funcionario: " + usuario.getNome() +
-				"=============\n");
+		
+		buffer.write("============== Relatorio de vendas do(a) funcionario(a): " + usuario.getNome() +
+				" =============\n");
 		for (int i=0; i< vendas.size(); i++){
 			if(usuario.getCpf().equals(vendas.get(i).getUsuario().getCpf())){
 				temVenda = true;
@@ -37,6 +38,80 @@ public class Relatorio {
 		if(temVenda == false){
 			buffer.write("O usuario " + usuario.getNome() + " nao tem vendas.\n");
 		}
+		
+		buffer.flush();
+		arquivo.close();
+	}
+	
+/*	public void gerarVendasTodosFuncionarios(List<Usuario> usuarios, List<Venda> vendas) throws IOException{
+		Writer arquivo = new FileWriter("VendasTotais.txt");
+		BufferedWriter buffer = new BufferedWriter(arquivo);
+				
+		boolean temVenda = false;
+		DecimalFormat valorFormat = new DecimalFormat("#.00");
+		
+		buffer.write("============== Relatorio total de vendas ==============\n");
+		
+		for (int i=0; i<usuarios.size(); i++) {
+			buffer.write("\n============== Funcionario(a): " + usuarios.get(i).getNome() + " ==============\n");
+			double valorTotal = 0;
+			
+			for (int j=0; j<vendas.size(); j++) {
+				if (usuarios.get(i).getCpf().equals(vendas.get(j).getUsuario().getCpf())){
+					temVenda = true;
+					double valorVenda = vendas.get(j).getValorLiquido();
+					valorVenda = (valorVenda*1.0738);
+					valorTotal = valorTotal + valorVenda;
+				}
+				
+				if(temVenda == false){
+					buffer.write("O usuario " + usuarios.get(i).getNome() + " nao tem vendas. \n");
+				} else {
+					buffer.write("Valor total das vendas: R$ " + valorFormat.format(valorTotal) + "\n");
+				}
+			}	
+		} 
+		
+		buffer.flush();
+		arquivo.close();
+	}
+	*/
+	
+	public void gerarVendasTodosFuncionarios(List<Usuario> usuarios, List<Venda> vendas) throws IOException{
+		Writer arquivo = new FileWriter("VendasTotais.txt");
+		BufferedWriter buffer = new BufferedWriter(arquivo);
+		
+		double todosFuncionarios = 0;
+		boolean temVenda = false;
+		DecimalFormat valorFormat = new DecimalFormat("#.00");
+		
+		buffer.write("============== Relatorio total de vendas ==============\n");
+		
+		for (int i=0; i<usuarios.size(); i++) {
+			double valorTotal = 0;
+			temVenda = false;
+			
+			buffer.write("\n============== Funcionario(a): " + usuarios.get(i).getNome() + " ==============\n");
+			
+			for (int j=0; j<vendas.size(); j++) {
+				if (usuarios.get(i).getCpf().equals(vendas.get(j).getUsuario().getCpf())){
+					temVenda = true;
+					double valorVenda = vendas.get(j).getValorLiquido();
+					valorVenda = (valorVenda*1.0738);
+					valorTotal = valorTotal + valorVenda;
+				}	
+			}	
+		
+			todosFuncionarios += valorTotal;
+			
+			if(temVenda == false){
+				buffer.write("O usuario " + usuarios.get(i).getNome() + " nao tem vendas. \n");
+			} else {
+				buffer.write("Valor total das vendas: R$ " + valorFormat.format(valorTotal) + "\n");
+			}
+		}
+		buffer.write ("\n============== Somatoria das vendas ==============");
+		buffer.write ("\nValor total: R$ " + valorFormat.format(todosFuncionarios));
 		
 		buffer.flush();
 		arquivo.close();
